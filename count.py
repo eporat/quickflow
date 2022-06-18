b6eb02c9843d15da99e4f6411ea9b323bdf8fa20
eporat <ethan.porat@gmail.com>
2022-06-18 22:45:34 +0300

    made count actually sublogarithmic

--- count.py
+++ count.py
@@ -2,11 +2,12 @@ from functions import Mean, Median
 from stream import StreamAlgorithm, create_group
 import random
 from math import log2
+from numpy import uint8
 
 class MorrisAlgorithm(StreamAlgorithm[int]):
     def __init__(self) -> None:
         super().__init__()
-        self.X = 0
+        self.X: uint8 = 0
 
     def update(self, _) -> None:
         if random.random() <= 1.0 / 2**self.X:
--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from sketch import AMSSketch
 from stream import Stream, create_group
 from count import ApproximateCounting
 
-a: Stream = enumerate(iter([1] * 10000))
+a: Stream = enumerate(iter([1] * 1_000))
 algorithm1 = ApproximateCounting(epsilon=0.5, delta=0.01)
-algorithm2 = ApproximateCounting(epsilon=0.1, delta=0.01)
+algorithm2 = ApproximateCounting(epsilon=1./3, delta=0.01)
 ams = Mean(create_group(AMSSketch, 100))
 
-print(create_group([algorithm1, algorithm2, ams]).run(a))
 print(len(algorithm1))
 print(len(algorithm2))
 print(len(ams))
+
+print(create_group([algorithm1, algorithm2, ams]).run(a))
