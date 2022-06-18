0f66e22475287ce64024d25b9a8ca634cbab781d
eporat <ethan.porat@gmail.com>
2022-06-19 02:11:38 +0300

    countsketch

--- sketch.py
+++ sketch.py
@@ -1,5 +1,5 @@
 from math import ceil, log2
-from functions import Mean, Min
+from functions import Mean, Median, Min
 from math_utils import KWiseIndependentGenerator, md5, random128
 from stream import Element, StreamAlgorithm, create_group
 
@@ -30,3 +30,18 @@ class CountMin(StreamAlgorithm[float]):
         return self.s[self.h(i)] 
 
 CountMinPlus = lambda epsilon, n: Min(create_group(CountMin, ceil(log2(n)), epsilon=epsilon))
+CountMinPlusPlus = lambda epsilon, n: Median(create_group(CountMinPlus, ceil(log2(n)), epsilon=epsilon, n=n))
+
+class CountSketch(StreamAlgorithm[float]):
+    def __init__(self, epsilon) -> None:
+        self.w = ceil(4/epsilon**2)
+        self.r = random128()
+        self.h = lambda x: md5(x ^ self.r) % self.w
+        self.s = [0] * self.w
+        self.random = KWiseIndependentGenerator(k=4)
+
+    def update(self, element: Element):
+        self.s[self.h(element[0])] += element[1] * (2 * (self.random(element[0]) % 2) - 1)
+
+    def __call__(self, i: int) -> float:
+        return self.s[self.h(i)] * (2 * (self.random(i) % 2) - 1)
